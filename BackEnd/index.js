db70d54f0bfbd63d0e7481ee3868481fab0de392
IsmaelSailema20 <ismaelsailema110@gmail.com>
2024-12-05 23:22:00 -0500

    Estructura inicial del proyecto

---
+++ BackEnd/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+// Habilitar CORS para todas las rutas
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+  });
